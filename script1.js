2700650e66aa59959d3994350283da85f6af849f
Rahul Karki <51796932+RahulKarki98@users.noreply.github.com>
2022-06-14 22:31:55 +0530

    Create script1.js
    JSON RESUME

---
+++ script1.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "RAHUL KARKI",
+      "email": "karkirahul@gamil.com",
+      "phone": 8755467583,
+      "degree": "B.tech",
+      "location": {
+        "address": "Avash Vikash",
+        "postalCode": 249201,
+        "city": "Rishikesh",
+        "state": "Uttarakhand",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/rahulkarki/",
+          "github":"https://github.com/RAHUlKARKI-1"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "The Time",
+        "position": "Developer",
+        "startDate": "2022-1-04",
+        "endDate": "2023-08-15",
+        "summary": "i am the full stack developer handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "College Of Engineering",
+        "department": "C.S.E",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2020,
+        "gpa": 7.4,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": [
+          "facial detection system using python language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Hindi,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "listening songs,",
+      }
+    ]
+  }
+  console.log(myResume);
